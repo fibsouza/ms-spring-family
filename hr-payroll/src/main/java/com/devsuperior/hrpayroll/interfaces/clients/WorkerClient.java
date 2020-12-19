2e3b5d5221f6f48c59927d82ff0de6a0020f361c
Franciele Barros <franbarros@ibm.com>
2020-12-19 17:41:36 -0300

    adc uso do Ribbon

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/interfaces/clients/WorkerClient.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/interfaces/clients/WorkerClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Component
-@FeignClient(name="hr-worker", url = "${integration.hr-worker.baseUrl}", path = "/worker")
+@FeignClient(name="hr-worker", path = "/worker")
 public interface WorkerClient {
 
     @GetMapping("/{id}")
