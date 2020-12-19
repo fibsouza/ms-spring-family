e591ca58333af368212a7b5bc5665c3385469815
Franciele Barros <franbarros@ibm.com>
2020-12-19 16:10:17 -0300

    implementacao de feign

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
@@ -2,30 +2,22 @@ package com.devsuperior.hrpayroll.services;
 
 import com.devsuperior.hrpayroll.entities.Payment;
 import com.devsuperior.hrpayroll.entities.Worker;
+import com.devsuperior.hrpayroll.interfaces.clients.WorkerClient;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.HashMap;
-import java.util.Map;
-
-@Service
 @AllArgsConstructor
 @NoArgsConstructor
+@Service
 public class PaymentService {
 
-    @Value("${integration.hr-worker.baseUrl}")
-    private String baseUrl;
-
-    RestTemplate client = new RestTemplate();
+    @Autowired
+    private WorkerClient client;
 
     public Payment getPayment(Long workerId, Integer days){
-        Map<String, Long> params = new HashMap<>();
-        params.put("id", workerId);
-        Worker worker = client.getForObject(baseUrl +"/worker/{id}", Worker.class, params);
-
+        Worker worker = client.findById(workerId).getBody();
         return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
