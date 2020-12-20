9570da60a6d6d863eb382fb124c09a2cb8c86b61
Franciele Barros <franbarros@ibm.com>
2020-12-20 13:09:04 -0300

    hr-worker deletado

--- hr-worker/src/main/resources/data.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO tb_worker (name, daily_Income) VALUES ('Jose', 230.0);
-INSERT INTO tb_worker (name, daily_Income) VALUES ('Maria', 80.0);
-INSERT INTO tb_worker (name, daily_Income) VALUES ('Alex', 150.0);
-INSERT INTO tb_worker (name, daily_Income) VALUES ('Bob', 200.0);
-INSERT INTO tb_worker (name, daily_Income) VALUES ('Antonio', 320.0);
-INSERT INTO tb_worker (name, daily_Income) VALUES ('Luiza', 450.0);
-INSERT INTO tb_worker (name, daily_Income) VALUES ('Marcela', 200.0);
-INSERT INTO tb_worker (name, daily_Income) VALUES ('Deodette', 100.0);
-INSERT INTO tb_worker (name, daily_Income) VALUES ('Camilo', 50.0);
