18176d08a596e5d7b3db7736fc2492db515939e1
Franciele Barros <franbarros@ibm.com>
2020-12-20 15:53:00 -0300

    aplicacao de exemplo de toletancia a falhas com hystrix

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/interfaces/PaymentController.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/interfaces/PaymentController.java
@@ -2,6 +2,7 @@ package com.devsuperior.hrpayroll.interfaces;
 
 import com.devsuperior.hrpayroll.entities.Payment;
 import com.devsuperior.hrpayroll.services.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,9 +17,15 @@ public class PaymentController {
 
     private final PaymentService service;
 
+    @HystrixCommand(fallbackMethod = "getPaymentAlternative")
     @GetMapping(value = "/{employeeId}/days/{days}")
     public ResponseEntity<Payment> getPayment(@PathVariable Long employeeId, @PathVariable Integer days){
         Payment payment = service.getPayment(employeeId, days);
+        return ResponseEntity.ok(payment);
+    }
+
+    public ResponseEntity<Payment> getPaymentAlternative(Long employeeId, @PathVariable Integer days){
+        Payment payment = new Payment("Default", 200, 1);
         return ResponseEntity.ok(payment);
     }
 }
